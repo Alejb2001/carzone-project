fb7878880bf5cd7a9b5d79e1183d50abfed7aefa
Alejandro Burciaga Calzadillas <alejbur@gmail.com>
2025-08-27 11:03:58 -0600

    Creacrión de paginas para informacion de la empresa, contacto y servicios

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Ruta para la página de inicio
+    path('about', views.about, name='about'),  # Ruta para la página "Acerca de"
+    path('services', views.services, name='services'),  # Ruta para la página de servicios
+    path('contact', views.contact, name='contact'),  # Ruta para la página de contacto
 ]
+
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/home.html')  # Renderiza la plantilla 'home.html'
+
+def about(request):
+    return render(request, 'pages/about.html')  # Renderiza la plantilla 'about.html'
+
+def services(request):
+    return render(request, 'pages/services.html')  # Renderiza la plantilla 'services.html'
+
+def contact(request):
+    return render(request, 'pages/contact.html')  # Renderiza la plantilla 'contact.html'
